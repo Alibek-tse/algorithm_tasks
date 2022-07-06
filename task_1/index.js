ef8f9608285359c6c512b3b6d147730a55c98f85
alibek-tse <tokanov.a.k@gmail.com>
2022-07-07 01:12:46 +0600

    first task

---
+++ task_1/index.js
@@ -0,0 +1,8 @@
+function isUnique(string) {
+  return new Set(string).size === string.length ? true : false;
+}
+
+console.log(isUnique("abcdef")); // -> true
+console.log(isUnique("1234567")); // -> true
+console.log(isUnique("abcABC")); // -> true
+console.log(isUnique("abcadef")); // -> false
