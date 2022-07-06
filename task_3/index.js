5cd9e8b50ffd4d4ef64af309ec5a4b4a0d661741
alibek-tse <tokanov.a.k@gmail.com>
2022-07-07 01:42:30 +0600

    task 3

---
+++ task_3/index.js
@@ -0,0 +1,8 @@
+function removeDupes(str) {
+  return Array.from(new Set(str)).join();
+}
+
+console.log(removeDupes("abcd")); // -> 'abcd'
+console.log(removeDupes("aabbccdd")); // -> 'abcd'
+console.log(removeDupes("abcddbca")); // -> 'abcd'
+console.log(removeDupes("abababcdcdcd")); // -> 'abcd'
